ac7c749da430e6c64cef1db3370c74fd9a463b11
Dinohund <Tom@MacBook-Pro.local>
2016-02-24 02:00:41 +0100

    ggT hinzugefügt

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -1,11 +1,44 @@
 package test;
 
 public class TestClass {
+	
+	public static boolean outputEnabled = true;
 
 	public static void main(String[] args) {
 		
 		System.out.println(fak(5));
 		System.out.println(fib(5));
+		ggT(654321, 123456);
+	}
+	
+	public static int ggT(int a, int b) {
+		
+		if (a == b) {
+			return a;
+		}
+		else if (a < b) {
+			int temp = b;
+			b = a;
+			a = temp;
+		}
+		
+		int result = 0;
+		
+		while (a%b != 0) {
+				
+			result = a%b;
+			
+			if (outputEnabled) {
+				System.out.println(b+":"+result);
+			}
+	
+			if (result != 0) {
+				a = b;
+				b = result;
+			}
+		}
+		
+		return result;
 	}
 	
 	//Fibonaccizahlen (rekursiv)
