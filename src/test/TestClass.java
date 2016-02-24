0f882c49bfe2a70965995f9e3218f141530b0303
Dinohund <Tom@MacBook-Pro.local>
2016-02-24 01:31:16 +0100

    Fakultät hinzugefügt
    (rekursiv)

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -3,8 +3,22 @@ package test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("TEst!");
+		System.out.println(fak(30));
 
+	}
+	
+	//Fakultätsfunktion (rekursiv)
+	public static int fak(int n) {
+		
+		int result = n;
+		
+		if (n == 0) {
+			result = 1;
+		}
+		else {
+			result = n*fak(n-1);
+		}
+		return result;
 	}
 
 }
