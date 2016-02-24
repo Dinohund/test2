a2267145e9f4c26f17dc6969c8701a0c689c3870
Dinohund <Tom@MacBook-Pro.local>
2016-02-24 01:40:27 +0100

    fibonacci zahlen hinzugefügt

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -3,14 +3,33 @@ package test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println(fak(30));
-
+		
+		System.out.println(fak(5));
+		System.out.println(fib(5));
+	}
+	
+	//Fibonaccizahlen (rekursiv)
+	public static int fib(int n) {
+		
+		int result = 0;
+		
+		if (n == 1) {
+			result = 1;
+		}
+		else if (n == 2) {
+			result = 1;
+		}
+		else {
+			result = n+fib(n-1);
+		}
+		
+		return result;
 	}
 	
 	//Fakultätsfunktion (rekursiv)
 	public static int fak(int n) {
 		
-		int result = n;
+		int result = 0;
 		
 		if (n == 0) {
 			result = 1;
